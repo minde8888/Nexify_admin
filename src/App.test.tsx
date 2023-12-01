186f91daeb93e594c5d05c450db0d6f938631c5b
minde8888 <mindebaltru@gmail.com>
2023-12-01 10:52:37 +0100

    jest setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,14 +2,14 @@ import App from './App';
 import { renderBrowserWithContext } from './testUtils/RenderBrowserWithContext';
 
 jest.mock('react-markdown', () => ({
+    ...jest.requireActual('react-markdown'), 
     Markdown: ({ children }: any) => <div>{children}</div>,
-    defaultUrlTransform: jest.fn(),
-}));
+  }));
+  
 
 describe('<App />', () => {
-    test('renders', () => {
-        const { baseElement } = renderBrowserWithContext(<App />);
-        expect(baseElement).toBeVisible();
-    });
+  test('renders', () => {
+    const { baseElement } = renderBrowserWithContext(<App />);
+    expect(baseElement).toBeVisible();
+  });
 });
-
